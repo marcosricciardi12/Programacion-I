e5a4bb01e3c70ed43fcfa8395e2fba9ef5e9469b
marcosricciardi12 <marcosricciardi12@gmail.com>
2022-03-28 08:59:34 -0300

    Cambio de variables

--- backend/main/resources/Poem.py
+++ backend/main/resources/Poem.py
@@ -1,8 +1,6 @@
 from flask_restful import Resource
 from flask import request
 
-#Diccionario de prueba
-
 POEMS = {
     1: {'title': 'Hola', 'date': '10/12/2020'},
     2: {'title': 'Chau', 'date': '11/10/2021'},
@@ -18,20 +16,19 @@ class Poem(Resource):
 
     #Eliminar recurso
     def delete(self, id):
-        #Verificar que exista un Profesor con ese Id en diccionario
         if int(id) in POEMS:
-            #Eliminar professor del diccionario
             del POEMS[int(id)]
             return '', 204
         return '', 404
+
     #Modificar recurso
     def put(self, id):
         if int(id) in POEMS:
-            professor = POEMS[int(id)]
+            poem = POEMS[int(id)]
             #Obtengo los datos de la solicitud
             data = request.get_json()
-            professor.update(data)
-            return professor, 201
+            poem.update(data)
+            return poem, 201
         return '', 404
 
 #Recurso Poems
@@ -42,8 +39,8 @@ class Poems(Resource):
     #Insertar recurso
     def post(self):
         #Obtener datos de la solicitud
-        professor = request.get_json()
+        poem = request.get_json()
         id = int(max(POEMS.keys())) + 1
-        POEMS[id] = professor
+        POEMS[id] = poem
         return POEMS[id], 201
 

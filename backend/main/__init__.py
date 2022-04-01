d335b9587b03e53d06b1693c27a072744a5085e5
marcosricciardi12 <marcosricciardi12@gmail.com>
2022-04-01 12:19:29 -0300

    Agregado flask_alchemy

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -2,12 +2,21 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
-import main.resources as resources
+from flask_sqlalchemy import SQLAlchemy
 api = Api()
+db = SQLAlchemy()
+
 
 def create_app():
 	app = Flask(__name__)
 	load_dotenv()
+
+	if not os.path.exists(os.getenv('DATABASE_PATH') + os.getenv('DATABASE_NAME')):
+		os.mknod(os.getenv('DATABASE_PATH') + os.getenv('DATABASE_NAME'))
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
+	db.init_app(app)
+	import main.resources as resources
 	api.add_resource(resources.PoemsResource, '/poems')
 	api.add_resource(resources.PoemResource, '/poem/<id>')
 	api.add_resource(resources.UsersResource, '/users')
